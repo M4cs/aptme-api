8ba200dcfc20bf20eabe1fe3ef1bbb386d5b428e
M4cs <mabridgland@protonmail.com>
2019-07-02 13:53:20 -0400

    fadsfasdf

--- app/controllers/rootrender.py
+++ app/controllers/rootrender.py
@@ -18,7 +18,7 @@ class RootRender:
                         if "kiimo" not in k:
                             if "xarold" not in k:
                                 if "pulandres" not in k:
-                                    if "http://cool" != k:
+                                    if "https://cool" != k:
                                         entry += template.format(count=count, link=k, vc=v)
                                         count += 1
         return entry

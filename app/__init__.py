dce02ac6c44514067dbed7d03f0e5c1e164b82fe
M4cs <mabridgland@protonmail.com>
2019-07-02 12:12:08 -0400

    add

--- app/__init__.py
+++ app/__init__.py
@@ -74,17 +74,17 @@ def manifest():
     "start_url": "/",
     "icons": [
         {
-            "src": "./android-chrome-192x192.png",
+            "src": "./assets/img/android-chrome-192x192.png",
             "sizes": "192x192",
             "type": "image/png"
         },
         {
-            "src": "./android-chrome-512x512.png",
+            "src": "./assets/img/android-chrome-512x512.png",
             "sizes": "512x512",
             "type": "image/png"
         },
         {
-            "src": "./apple-touch-icon.png",
+            "src": "./assets/img/apple-touch-icon.png",
             "sizes": "512x512",
             "type": "image/png"
         },

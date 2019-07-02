58a4cfd00ca0b7758f56e4145cd8f755d07f78d4
M4cs <mabridgland@protonmail.com>
2019-07-02 15:23:28 -0400

    fad

--- app/controllers/rootrender.py
+++ app/controllers/rootrender.py
@@ -6,7 +6,7 @@ class RootRender:
     def generateLeaderboard():
         template = """\
           <a href="https://aptme.io/api/repos?search={link}"><h3 class="subtitle">{count}. {link}</h3>
-          <h3 class="subtitle"><i class="fas fa-eye"></i> {vc}</h3></a>
+          <h3 class="subtitle"><i class="fas fa-eye" style="margin-right: 1px;"></i> {vc}</h3></a>
           <hr>"""
         entry = ""
         count = 1

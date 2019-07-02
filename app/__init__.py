11d8978d41c42f3ea55d26b795ad0861a9f36195
M4cs <mabridgland@protonmail.com>
2019-07-02 16:06:57 -0400

    aaa

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ api = Api(app)
 limiter = Limiter(
     app,
     key_func=get_remote_address,
-    default_limits=['100 per minute']
+    default_limits=['5000 per minute']
 )
 
 @app.route('/api')

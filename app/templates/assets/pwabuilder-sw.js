fd8e4a3976c820d31310226538172daddd1a7dcd
M4cs <mabridgland@protonmail.com>
2019-07-02 13:34:27 -0400

    fadsfad

--- app/templates/assets/pwabuilder-sw.js
+++ app/templates/assets/pwabuilder-sw.js
@@ -13,7 +13,7 @@ self.addEventListener("install", function (event) {
     caches.open(CACHE).then(function (cache) {
       console.log("[PWA Builder] Cached offline page during install");
 
-      if (offlineFallbackPage === "ToDo-replace-this-name.html") {
+      if (offlineFallbackPage === "index.html") {
         return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
       }
 

67eabb4b86f5c0f3b843769639d6762a72bef484
M4cs <mabridgland@protonmail.com>
2019-07-03 17:30:53 +0000

    :qRevert "Remove Print"
    This reverts commit 3f2beddf6ac481231258025e8caa5b3ad470f956.

--- app/controllers/search.py
+++ app/controllers/search.py
@@ -56,6 +56,7 @@ def get_packages(link):
 def packages_to_json(link, packages):
     if packages == None:
         return None
+    print(packages)
     list_of_entries = packages.decode('utf-8').splitlines()
     package_json = {}
     package_ids = []

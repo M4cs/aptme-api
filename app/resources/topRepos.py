197700c2ec9014e0fa0f142c0144b2d3d9544cb9
M4cs <mabridgland@protonmail.com>
2019-07-03 00:36:53 -0400

    aaa

--- app/resources/topRepos.py
+++ app/resources/topRepos.py
@@ -5,5 +5,5 @@ class TopRepos(Resource):
     def get(self):
         with open('app/db/link_count.json', 'r+') as json_file:
             data = json.load(json_file)
-            sortdict = [(k, data[k]) for k in sorted(data, key=data.get, reverse=True)]
+            sortdict = [(k, data[k]) for k in sorted(data, key=data.get, reverse=True) if 'kiiimo' not in k]
         return sortdict

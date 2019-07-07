12c0a9fba469a9b3c01f43a521e7a8884c0a8718
M4cs <mabridgland@protonmail.com>
2019-07-07 00:07:08 -0400

    Add UDID and fix download py

--- app/resources/download.py
+++ app/resources/download.py
@@ -1,11 +1,11 @@
 from flask_restful import Resource, reqparse
-from flask import redirect
+from flask import redirect, request
 import requests
 
 def download_parser():
     parser = reqparse.RequestParser()
     parser.add_argument('d', required=True)
-    parser.add_argument('l', required=True)
+    parser.add_argument('udid', required=True)
     return parser
 
 class Download(Resource):
@@ -13,16 +13,16 @@ class Download(Resource):
         parser = download_parser()
         data = parser.parse_args()
         dl_link = data['d']
-        host_link = data['l']
+        udid = data['udid']
         headers = {
             'User-Agent': 'Sileo/1 CFNetwork/976 Darwin/18.2.0',
             'X-Firmware': '12.1.2',
             'X-Machine': 'iPhone10,3',
-            'X-Unique-ID': 'df2b5bc80c02907c03dc65e9f38eedfa350711bb',
-            'Authority': host_link,
+            'X-Unique-ID': udid,
             'Accept': '*/*',
             'Keep-Alive': 'True'
         }
-        response = redirect(dl_link, 200)
-        response.headers = headers
+        response = request.Response(headers=headers,
+                            is_redirect=True,
+                            url=dl_link)
         return response
